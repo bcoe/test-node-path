04e362ee0a59bbb0d59170a3df1d1158b48905c0
Benjamin Coe <ben@npmjs.com>
2016-01-31 20:54:32 -0800

    trying to get some info about what's going on

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(JSON.stringify(require.main.filename) + JSON.stringify(require.main.children) + '\n')
+  res.end(JSON.stringify(require.main.filename) + require.main.children[0] + '\n')
 }).listen(port);

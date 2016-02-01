e71348804ea8226b011e0d291949a3c1e58cbc24
Benjamin Coe <ben@npmjs.com>
2016-01-31 20:44:37 -0800

    why ain't I serving

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n' + JSON.stringify(require.main));
+  // res.end('Hello World\n' + JSON.stringify(require.main));
+  res.end('Hello World\n')
 }).listen(port);

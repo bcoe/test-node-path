e19c51e8c0d6bfb322ed67596ef07ca96d1dcfb4
Benjamin Coe <ben@npmjs.com>
2016-01-31 22:07:23 -0800

    test using yargs

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
-var pkgConf = require('pkg-conf')
+var yargs = require('yargs')
 
 http.createServer(function(req, res) {
-  var conf = pkgConf.sync('yargs', {
-    defaults: {},
-    cwd: require.main.children[0].filename
-  })
+  var argv = yargs('--config ./config.json')
+    .config('config')
+    .argv
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(JSON.stringify(conf) + '\n')
+  res.end(JSON.stringify(argv) + '\n')
 }).listen(port);

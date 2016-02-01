3564dfaf03a8310d864141c1754a51232c9cbf29
Benjamin Coe <ben@npmjs.com>
2016-01-31 21:06:44 -0800

    I expect this not to find a module

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
+var pkgConf = require('pkg-conf')
+
 http.createServer(function(req, res) {
+  var conf = pkgConf.sync('yargs', {
+    defaults: {},
+    cwd: require.main.filename
+  })
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(JSON.stringify(require.main.filename) + require.main.children[0] + '\n')
+  res.end(JSON.stringify(conf) + '\n')
 }).listen(port);
